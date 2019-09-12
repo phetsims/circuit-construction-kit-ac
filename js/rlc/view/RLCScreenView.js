90b0e75f9617651c3c09ca0ee17b178c66f284a4
samreid <reids@colorado.edu>
2019-09-12 10:34:13 -0600

    Fix component list for RLC screen

--- js/rlc/view/RLCScreenView.js
+++ js/rlc/view/RLCScreenView.js
@@ -13,7 +13,6 @@ define( require => {
   const CCKCScreenView = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/view/CCKCScreenView' );
   const circuitConstructionKitAcAndDc = require( 'CIRCUIT_CONSTRUCTION_KIT_AC_AND_DC/circuitConstructionKitAcAndDc' );
   const CircuitElementToolFactory = require( 'CIRCUIT_CONSTRUCTION_KIT_COMMON/view/CircuitElementToolFactory' );
-  const Node = require( 'SCENERY/nodes/Node' );
   const NumberProperty = require( 'AXON/NumberProperty' );
   const TimerNode = require( 'SCENERY_PHET/TimerNode' );
 
@@ -27,32 +26,18 @@ define( require => {
       const circuitElementToolFactory = new CircuitElementToolFactory( model.circuit, model.showLabelsProperty, model.viewTypeProperty,
         point => this.circuitLayerNode.globalToLocalPoint( point ) );
 
-      const wireToolNode = circuitElementToolFactory.createWireToolNode( 25, tandem.createTandem( 'wireToolNode' ) );
-
       // Tool nodes that appear on every screen. Pagination for the carousel, each page should begin with wire node
       const circuitElementToolNodes = [
 
         // This page is duplicated in the Lab Screen View
-        wireToolNode,
+        circuitElementToolFactory.createWireToolNode( 25, tandem.createTandem( 'wireToolNode' ) ),
         circuitElementToolFactory.createRightBatteryToolNode( 10, tandem.createTandem( 'rightBatteryToolNode' ) ),
         circuitElementToolFactory.createACVoltageToolNode( 10, tandem.createTandem( 'rightBatteryToolNode' ) ),
         circuitElementToolFactory.createLightBulbToolNode( 10, tandem.createTandem( 'lightBulbToolNode' ) ),
         circuitElementToolFactory.createResistorToolNode( 10, tandem.createTandem( 'resistorToolNode' ) ),
-        circuitElementToolFactory.createSwitchToolNode( 5, tandem.createTandem( 'switchToolNode' ) ),
-        // circuitElementToolFactory.createCapacitorToolNode( 10, tandem.createTandem( 'capacitorToolNode' ) ),
-        // circuitElementToolFactory.createInductorToolNode( 10, tandem.createTandem( 'inductorToolNode' ) ),
-
-
-        new Node( { children: [ wireToolNode ] } ), // Wire should appear at the top of each carousel page
-        circuitElementToolFactory.createDollarBillToolNode( 1, tandem.createTandem( 'dollarBillToolNode' ) ),
-        circuitElementToolFactory.createPaperClipToolNode( 1, tandem.createTandem( 'paperClipToolNode' ) ),
-        circuitElementToolFactory.createCoinToolNode( 1, tandem.createTandem( 'coinToolNode' ) ),
-        circuitElementToolFactory.createEraserToolNode( 1, tandem.createTandem( 'eraserToolNode' ) ),
-
-        new Node( { children: [ wireToolNode ] } ),// Wire should appear at the top of each carousel page
-        circuitElementToolFactory.createPencilToolNode( 1, tandem.createTandem( 'pencilToolNode' ) ),
-        circuitElementToolFactory.createHandToolNode( 1, tandem.createTandem( 'handToolNode' ) ),
-        circuitElementToolFactory.createDogToolNode( 1, tandem.createTandem( 'dogToolNode' ) )
+        circuitElementToolFactory.createCapacitorToolNode( 10, tandem.createTandem( 'capacitorToolNode' ) ),
+        circuitElementToolFactory.createInductorToolNode( 10, tandem.createTandem( 'inductorToolNode' ) ),
+        circuitElementToolFactory.createSwitchToolNode( 5, tandem.createTandem( 'switchToolNode' ) )
       ];
 
       super( model, circuitElementToolNodes, tandem, {
@@ -61,7 +46,9 @@ define( require => {
         showResistivityControl: false,
         showBatteryResistanceControl: false,
         showCharts: true,
-        itemsPerPage: 6
+        itemsPerPage: circuitElementToolNodes.length,
+        pageHeight: 400,
+        hideDisabledButtons: true
       } );
 
       // @public - the TimerNode
