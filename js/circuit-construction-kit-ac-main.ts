b652a982186551c5bb8491d648d2f4eccbbf5daa
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-11 16:57:52 -0600

    add CCKCSim and fix iframe keyboard bug in all CCK sims, https://github.com/phetsims/circuit-construction-kit-common/issues/1027

--- js/circuit-construction-kit-ac-main.ts
+++ js/circuit-construction-kit-ac-main.ts
@@ -6,9 +6,10 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import CCKCSim from '../../circuit-construction-kit-common/js/view/CCKCSim.js';
 import CCKCSimulationPreferencesContentNode from '../../circuit-construction-kit-common/js/view/CCKCSimulationPreferencesContentNode.js';
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import Sim, { SimOptions } from '../../joist/js/Sim.js';
+import { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
@@ -42,7 +43,7 @@ const simOptions: SimOptions = {
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
-  const sim = new Sim( circuitConstructionKitAcTitleStringProperty, [
+  const sim = new CCKCSim( circuitConstructionKitAcTitleStringProperty, [
     new ACVoltageScreen( tandem.createTandem( 'acVoltageScreen' ) ),
     new RLCScreen( tandem.createTandem( 'rlcScreen' ) ),
     new LabScreen( tandem.createTandem( 'labScreen' ) )
