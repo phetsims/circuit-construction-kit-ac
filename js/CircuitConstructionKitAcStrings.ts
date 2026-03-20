f648e14252d25e030a2343e3d787ae1fd67abd6f
phet-dev <phetadmin@gmail.com>
2026-03-20 13:54:42 -0600

    grunt update  from daily grunt work

--- js/CircuitConstructionKitAcStrings.ts
+++ js/CircuitConstructionKitAcStrings.ts
@@ -24,4 +24,6 @@ type StringsType = {
 
 const CircuitConstructionKitAcStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_AC' ) as StringsType;
 
+circuitConstructionKitAc.register( 'CircuitConstructionKitAcStrings', CircuitConstructionKitAcStrings );
+
 export default CircuitConstructionKitAcStrings;
